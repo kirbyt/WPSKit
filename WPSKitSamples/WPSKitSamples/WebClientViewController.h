b1233dc066324d13e563fd6ae98e342abd281660
Kirby Turner <kirby@thecave.com>
2012-02-16 20:24:48 -0500

    Added web client.

---
+++ WPSKitSamples/WPSKitSamples/WebClientViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebClientViewController.h
+//  WPSKitSamples
+//
+//  Created by Kirby Turner on 2/16/12.
+//  Copyright (c) 2012 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebClientViewController : UIViewController
+
+@end
---
+++ WPSKitSamples/WPSKitSamples/WebClientViewController.m
@@ -0,0 +1,13 @@
+//
+//  WebClientViewController.m
+//  WPSKitSamples
+//
+//  Created by Kirby Turner on 2/16/12.
+//  Copyright (c) 2012 White Peak Software Inc. All rights reserved.
+//
+
+#import "WebClientViewController.h"
+
+@implementation WebClientViewController
+
+@end
