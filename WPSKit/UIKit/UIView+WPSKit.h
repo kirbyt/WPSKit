498ccc83bac3d2715ac34ffcb36e66d8668367ad
Kirby Turner <kirby@thecave.com>
2013-05-05 19:32:08 -0400

    Add methods for auto layout constraints.

---
+++ WPSKit/UIKit/UIView+WPSKit.h
@@ -0,0 +1,49 @@
+/**
+ **   UIView+WPSKit.h
+ **
+ **   Created by Kirby Turner.
+ **   Copyright (c) 2012 White Peak Software. All rights reserved.
+ **
+ **   Permission is hereby granted, free of charge, to any person obtaining
+ **   a copy of this software and associated documentation files (the
+ **   "Software"), to deal in the Software without restriction, including
+ **   without limitation the rights to use, copy, modify, merge, publish,
+ **   distribute, sublicense, and/or sell copies of the Software, and to permit
+ **   persons to whom the Software is furnished to do so, subject to the
+ **   following conditions:
+ **
+ **   The above copyright notice and this permission notice shall be included
+ **   in all copies or substantial portions of the Software.
+ **
+ **   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ **   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ **   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ **   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ **   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ **   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ **   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ **
+ **/
+
+#import <UIKit/UIKit.h>
+
+typedef NS_OPTIONS(unsigned long, WPSViewPinEdges){
+    WPSViewPinTopEdge = 1 << 0,
+    WPSViewPinRightEdge = 1 << 1,
+    WPSViewPinBottomEdge = 1 << 2,
+    WPSViewPinLeftEdge = 1 << 3,
+    WPSViewPinAllEdges = ~0UL
+};
+
+@interface UIView (WPSKit)
+
+- (NSArray *)wps_pinToSuperviewEdges:(WPSViewPinEdges)edges inset:(CGFloat)inset;
+- (NSArray *)wps_pinToSuperviewEdgesWithInset:(UIEdgeInsets)insets;
+- (void)wps_removeAllConstraints;
+
+- (void)wps_constrainToHeight:(CGFloat)height;
+- (void)wps_constrainToWidth:(CGFloat)width;
+- (void)wps_constrainToHeightGreaterThanOrEqualTo:(CGFloat)height;
+- (void)wps_constrainToWidthGreaterThanOrEqualTo:(CGFloat)width;
+
+@end
---
+++ WPSKit/UIKit/UIView+WPSKit.m
@@ -0,0 +1,108 @@
+/**
+ **   UIView+WPSKit.m
+ **
+ **   Created by Kirby Turner.
+ **   Copyright (c) 2012 White Peak Software. All rights reserved.
+ **
+ **   Permission is hereby granted, free of charge, to any person obtaining
+ **   a copy of this software and associated documentation files (the
+ **   "Software"), to deal in the Software without restriction, including
+ **   without limitation the rights to use, copy, modify, merge, publish,
+ **   distribute, sublicense, and/or sell copies of the Software, and to permit
+ **   persons to whom the Software is furnished to do so, subject to the
+ **   following conditions:
+ **
+ **   The above copyright notice and this permission notice shall be included
+ **   in all copies or substantial portions of the Software.
+ **
+ **   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ **   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ **   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ **   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ **   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ **   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ **   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ **
+ **/
+
+#import "UIView+WPSKit.h"
+
+@implementation UIView (WPSKit)
+
+#pragma mark - Auto Layout Methods
+
+/**
+    The auto layout code is inspired and derived from the following 
+    open source projects:
+ 
+    https://github.com/jrturton/UIView-Autolayout
+ */
+
+- (NSArray *)wps_pinToSuperviewEdges:(WPSViewPinEdges)edges inset:(CGFloat)inset
+{
+    UIView *superview = [self superview];
+    NSAssert(superview,@"Cannot pin to a non-existing superview.");
+    
+    NSMutableArray *constraints = [NSMutableArray array];
+    
+    if (edges & WPSViewPinTopEdge)
+    {
+        [constraints addObject:[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:superview attribute:NSLayoutAttributeTop multiplier:1.0 constant:inset]];
+    }
+    if (edges & WPSViewPinLeftEdge)
+    {
+        [constraints addObject:[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:superview attribute:NSLayoutAttributeLeft multiplier:1.0 constant:inset]];
+    }
+    if (edges & WPSViewPinRightEdge)
+    {
+        [constraints addObject:[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:superview attribute:NSLayoutAttributeRight multiplier:1.0 constant:-inset]];
+    }
+    if (edges & WPSViewPinBottomEdge)
+    {
+        [constraints addObject:[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:superview attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-inset]];
+    }
+    [superview addConstraints:constraints];
+    return [constraints copy];
+}
+
+- (NSArray *)wps_pinToSuperviewEdgesWithInset:(UIEdgeInsets)insets
+{
+    NSAssert([self superview],@"Cannot pin to a non-existing superview.");
+    
+    NSMutableArray *constraints = [NSMutableArray new];
+    
+    [constraints addObjectsFromArray:[self wps_pinToSuperviewEdges:WPSViewPinTopEdge inset:insets.top]];
+    [constraints addObjectsFromArray:[self wps_pinToSuperviewEdges:WPSViewPinLeftEdge inset:insets.left]];
+    [constraints addObjectsFromArray:[self wps_pinToSuperviewEdges:WPSViewPinBottomEdge inset:insets.bottom]];
+    [constraints addObjectsFromArray:[self wps_pinToSuperviewEdges:WPSViewPinRightEdge inset:insets.right]];
+    
+    return [constraints copy];
+}
+
+- (void)wps_removeAllConstraints
+{
+    NSArray *constraints = [self constraints];
+    [self removeConstraints:constraints];
+}
+
+- (void)wps_constrainToHeight:(CGFloat)height
+{
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:0 constant:height]];
+}
+
+- (void)wps_constrainToWidth:(CGFloat)width
+{
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:0 constant:width]];
+}
+
+- (void)wps_constrainToHeightGreaterThanOrEqualTo:(CGFloat)height
+{
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationGreaterThanOrEqual toItem:nil attribute:0 multiplier:0 constant:height]];
+}
+
+- (void)wps_constrainToWidthGreaterThanOrEqualTo:(CGFloat)width
+{
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationGreaterThanOrEqual toItem:nil attribute:0 multiplier:0 constant:width]];
+}
+
+@end
