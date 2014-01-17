1bb4fcb8a7f893b9d5b7fd75d80adb221fab0471
Kirby Turner <kirby@thecave.com>
2014-01-17 16:58:32 -0500

    Add a couple of helpers for ui nav bars on a view controller.

---
+++ WPSKit/UIKit/UIViewController+WPSKit.m
@@ -0,0 +1,31 @@
+//
+//  UIViewController+WPSKit.m
+//  WPSKitSamples
+//
+//  Created by Kirby Turner on 1/17/14.
+//  Copyright (c) 2014 White Peak Software Inc. All rights reserved.
+//
+
+#import "UIViewController+WPSKit.h"
+
+@implementation UIViewController (WPSKit)
+
+- (void)wps_clearBackButtonTitle
+{
+  [self wps_setBackButtonTitle:@""];
+}
+
+- (void)wps_setBackButtonTitle:(NSString *)title
+{
+   UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:title style:UIBarButtonItemStyleBordered target:nil action:nil];
+   [[self navigationItem] setBackBarButtonItem:backButton];
+}
+
+- (void)wps_setBackButtonChevron:(UIImage *)image
+{
+  UINavigationBar *navBar = [[self navigationController] navigationBar];
+  [navBar setBackIndicatorImage:image];
+  [navBar setBackIndicatorTransitionMaskImage:image];
+}
+
+@end
