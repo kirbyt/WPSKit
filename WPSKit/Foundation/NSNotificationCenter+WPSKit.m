21570f8ee478a8ef484dde0c9630ca6f9e4956ee
Kirby Turner <kirby@thecave.com>
2014-04-07 11:17:20 -0400

    Add NSNotificationCenter category.

---
+++ WPSKit/Foundation/NSNotificationCenter+WPSKit.m
@@ -0,0 +1,43 @@
+//
+// NSNotificationCenter+WPSKit.h
+//
+// Created by Kirby Turner.
+// Copyright 2014 White Peak Software. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "NSNotificationCenter+WPSKit.h"
+
+@implementation NSNotificationCenter (WPSKit)
+
+- (void)wps_postOnMainThreadNotificationName:(NSString *)notificationName object:(id)notificationSender userInfo:(NSDictionary *)userInfo
+{
+	if (![NSThread isMainThread]) {
+		dispatch_async(dispatch_get_main_queue(), ^{
+			[self postNotificationName:notificationName object:notificationSender userInfo:userInfo];
+		});
+	}
+	else {
+		[self postNotificationName:notificationName object:notificationSender userInfo:userInfo];
+	}
+}
+
+@end
