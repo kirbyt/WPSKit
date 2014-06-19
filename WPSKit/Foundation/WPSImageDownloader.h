853a4859fbdcae933c8d5cbee3ef99243cdfccf6
Kirby Turner <kirby@thecave.com>
2014-06-19 09:51:42 -0400

    Change how scale factor is set for the downloaded image.

--- WPSKit/Foundation/WPSImageDownloader.h
+++ WPSKit/Foundation/WPSImageDownloader.h
@@ -1,29 +1,28 @@
-/**
- **   WPSImageDownloader
- **
- **   Created by Kirby Turner.
- **   Copyright 2011 White Peak Software. All rights reserved.
- **
- **   Permission is hereby granted, free of charge, to any person obtaining 
- **   a copy of this software and associated documentation files (the 
- **   "Software"), to deal in the Software without restriction, including 
- **   without limitation the rights to use, copy, modify, merge, publish, 
- **   distribute, sublicense, and/or sell copies of the Software, and to permit 
- **   persons to whom the Software is furnished to do so, subject to the 
- **   following conditions:
- **
- **   The above copyright notice and this permission notice shall be included 
- **   in all copies or substantial portions of the Software.
- **
- **   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
- **   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
- **   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- **   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
- **   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
- **   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
- **   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- **
- **/
+//
+// WPSImageDownloader.h
+//
+// Created by Kirby Turner.
+// Copyright 2011 White Peak Software. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 #import <Foundation/Foundation.h>
 #import "WPSCache.h"
@@ -36,6 +35,13 @@ typedef void(^WPSImageDownloaderCompletionBlock)(UIImage *image, NSURL *URL, NSE
 
 @property (nonatomic, strong, readonly) UIImage *image;
 @property (nonatomic, strong) id<WPSCache> cache;
+
+/**
+ The scale factor associated with the screen. Defaults to 1.0.
+ 
+ This should be set to [[UIScreen mainScreen] scale] when downloading retina veresion images. If you do not know if the image is a retina version then leave this property set to its default value, which is 1.0.
+ */
+@property (nonatomic, assign) CGFloat scale;
 
 - (void)downloadImageAtURL:(NSURL *)URL completion:(WPSImageDownloaderCompletionBlock)completion;
 
--- WPSKit/Foundation/WPSImageDownloader.m
+++ WPSKit/Foundation/WPSImageDownloader.m
@@ -1,29 +1,28 @@
-/**
- **   WPSImageDownloader
- **
- **   Created by Kirby Turner.
- **   Copyright 2011 White Peak Software. All rights reserved.
- **
- **   Permission is hereby granted, free of charge, to any person obtaining 
- **   a copy of this software and associated documentation files (the 
- **   "Software"), to deal in the Software without restriction, including 
- **   without limitation the rights to use, copy, modify, merge, publish, 
- **   distribute, sublicense, and/or sell copies of the Software, and to permit 
- **   persons to whom the Software is furnished to do so, subject to the 
- **   following conditions:
- **
- **   The above copyright notice and this permission notice shall be included 
- **   in all copies or substantial portions of the Software.
- **
- **   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
- **   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
- **   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- **   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
- **   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
- **   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
- **   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- **
- **/
+//
+// WPSImageDownloader.m
+//
+// Created by Kirby Turner.
+// Copyright 2011 White Peak Software. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 #import "WPSImageDownloader.h"
 #import "UIApplication+WPSKit.h"
@@ -38,6 +37,15 @@
 
 @implementation WPSImageDownloader
 
+- (instancetype)init
+{
+  self = [super init];
+  if (self) {
+    [self setScale:1.0];
+  }
+  return self;
+}
+
 - (void)downloadImageAtURL:(NSURL *)URL completion:(WPSImageDownloaderCompletionBlock)completion
 {
    if (URL) {
@@ -47,7 +55,7 @@
       if ([self cache]) {
          NSData *data = [[self cache] dataForKey:[self cacheKeyforURL:URL]];
          if (data) {
-            UIImage *image = [UIImage imageWithData:data scale:[[UIScreen mainScreen] scale]];
+            UIImage *image = [UIImage imageWithData:data scale:[self scale]];
             completion(image, URL, nil);
             return;
          }
@@ -101,7 +109,7 @@
 {
    [[UIApplication sharedApplication] wps_popNetworkActivity];
   
-   [self setImage:[UIImage imageWithData:[self receivedData] scale:[[UIScreen mainScreen] scale]]];
+   [self setImage:[UIImage imageWithData:[self receivedData] scale:[self scale]]];
     NSURL *URL = [[connection originalRequest] URL];
    if ([self cache]) {
       [[self cache] cacheData:[self receivedData] forKey:[self cacheKeyforURL:URL] cacheLocation:WPSCacheLocationFileSystem];
