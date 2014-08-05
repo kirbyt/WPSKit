9e6db2ded1b58388ca1c47d24519a3890f09fd74
Kirby Turner <kirby@thecave.com>
2014-08-05 16:02:18 -0400

    Add option to cache an existing file.
    This copies the file to the cache storage area.

--- WPSKit/Foundation/WPSCache.h
+++ WPSKit/Foundation/WPSCache.h
@@ -1,30 +1,28 @@
-/**
- **   WPSCache
- **
- **   Created by Kirby Turner.
- **   Copyright 2011 White Peak Software. All rights reserved.
- **
- **   Permission is hereby granted, free of charge, to any person obtaining 
- **   a copy of this software and associated documentation files (the 
- **   "Software"), to deal in the Software without restriction, including 
- **   without limitation the rights to use, copy, modify, merge, publish, 
- **   distribute, sublicense, and/or sell copies of the Software, and to permit 
- **   persons to whom the Software is furnished to do so, subject to the 
- **   following conditions:
- **
- **   The above copyright notice and this permission notice shall be included 
- **   in all copies or substantial portions of the Software.
- **
- **   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
- **   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
- **   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- **   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
- **   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
- **   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
- **   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- **
- **/
-
+//
+// WPSCache.h
+//
+// Created by Kirby Turner.
+// Copyright 2011 White Peak Software. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 #import <Foundation/Foundation.h>
 
@@ -60,6 +58,25 @@ typedef NS_ENUM(NSUInteger, WPSCacheLocation) {
  The cache age only applies to items cached to the file system.
  */
 - (void)cacheData:(NSData *)data forKey:(NSString *)key cacheLocation:(WPSCacheLocation)cacheLocation cacheAge:(NSInteger)cacheAge;
+
+/**
+ Caches the file at the provided location using the provided key.
+
+ Note that the file is cached for the maximum cache age as defined by `kWPSCacheMaxCacheAge`.
+ 
+ @param location The location of the file to cache. This is a file URL.
+ @param key The key used to identify this cache item.
+ */
+- (void)cacheFileAt:(NSURL *)location forKey:(NSString *)key;
+
+/**
+ Caches the file at the provided location using the provided key.
+ 
+ @param location The location of the file to cache. This is a file URL.
+ @param key The key used to identify this cache item.
+ @param cacheAge The time in seconds the file will remain in the cache before being flushed.
+ */
+- (void)cacheFileAt:(NSURL *)location forKey:(NSString *)key cacheAge:(NSInteger)cacheAge;
 
 /**
  Returns the data found in the cache for the key.
--- WPSKit/Foundation/WPSCache.m
+++ WPSKit/Foundation/WPSCache.m
@@ -1,34 +1,32 @@
-/**
- **   WPSCache
- **
- **   Created by Kirby Turner.
- **   Copyright 2011 White Peak Software. All rights reserved.
- **
- **   Permission is hereby granted, free of charge, to any person obtaining 
- **   a copy of this software and associated documentation files (the 
- **   "Software"), to deal in the Software without restriction, including 
- **   without limitation the rights to use, copy, modify, merge, publish, 
- **   distribute, sublicense, and/or sell copies of the Software, and to permit 
- **   persons to whom the Software is furnished to do so, subject to the 
- **   following conditions:
- **
- **   The above copyright notice and this permission notice shall be included 
- **   in all copies or substantial portions of the Software.
- **
- **   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
- **   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
- **   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- **   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
- **   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
- **   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
- **   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- **
- **
- **   Disclaimer:
- **   The following code is inspired by and derived from SDWebImage.
- **   http://github.com/rs/SDWebImage
- **
- **/
+//
+// WPSCache.m
+//
+// Created by Kirby Turner.
+// Copyright 2011 White Peak Software. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//  Disclaimer:
+//    The following code is inspired by and derived from SDWebImage.
+//    http://github.com/rs/SDWebImage
+//
 
 #import "WPSCache.h"
 #import <CommonCrypto/CommonDigest.h>
@@ -132,6 +130,33 @@
    
    if ((cacheLocation & WPSCacheLocationFileSystem) == WPSCacheLocationFileSystem) {
       [self persistData:data forKey:key withCacheAge:cacheAge];
+   }
+}
+
+- (void)cacheFileAt:(NSURL *)location forKey:(NSString *)key
+{
+   [self cacheFileAt:location forKey:key cacheAge:kWPSCacheMaxCacheAge];
+}
+
+- (void)cacheFileAt:(NSURL *)location forKey:(NSString *)key cacheAge:(NSInteger)cacheAge
+{
+   if (location == nil || [location isFileURL] == NO || key == nil) {
+      return;
+   }
+   
+   NSString *path = [self cachePathForKey:key];
+   
+   NSError *error = nil;
+   NSFileManager *fileManager = [[NSFileManager alloc] init];
+   if ([fileManager copyItemAtPath:[location path] toPath:path error:&error] == NO) {
+      NSLog(@"Failed to cache file. %@", [error localizedDescription]);
+   } else {
+      // The modified date is the expiration date for the cached item.
+      NSDate *expirationDate = [NSDate dateWithTimeIntervalSinceNow:cacheAge];
+      NSDictionary *modifiedDict = @{NSFileModificationDate: expirationDate};
+      if ([fileManager setAttributes:modifiedDict ofItemAtPath:path error:&error] == NO) {
+         NSLog(@"Failed to set file attributes. %@", [error localizedDescription]);
+      }
    }
 }
 
