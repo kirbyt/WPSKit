d8a2fac7dc8af9f0fbf6508c82faf4d3963336d9
Kirby Turner <kirby@thecave.com>
2015-05-26 09:10:59 -0400

    Add some handy date methods.

---
+++ WPSKit/Foundation/NSDate+WPSKit.m
@@ -0,0 +1,64 @@
+//
+// WPSKit
+// NSDate+WPSKit.m
+//
+// Created by Kirby Turner.
+// Copyright 2015 White Peak Software. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "NSDate+WPSKit.h"
+
+@implementation NSDate (WPSKit)
+
++ (NSDate *)wps_todayAtMidnight
+{
+  return [[NSDate date] wps_dateAtMidnight];
+}
+
+- (NSDate *)wps_dateAtMidnight
+{
+  NSDate *dateAtMidnight = nil;
+  
+  NSCalendar *calendar = [NSCalendar calendarWithIdentifier:NSGregorianCalendar];
+  if ([calendar respondsToSelector:@selector(startOfDayForDate:)]) {
+    // iOS 8 and above.
+    dateAtMidnight = [calendar startOfDayForDate:self];
+    
+  } else {
+    dateAtMidnight = [self wps_dateWithHour:0 minute:0 second:0];
+  }
+  
+  return dateAtMidnight;
+}
+
+- (NSDate *)wps_dateWithHour:(NSInteger)hour minute:(NSInteger)minute second:(NSInteger)second
+{
+  NSCalendar *calendar = [NSCalendar calendarWithIdentifier:NSGregorianCalendar];
+  NSDateComponents *dateComponents = [calendar components:NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit fromDate:self];
+  [dateComponents setHour:hour];
+  [dateComponents setMinute:minute];
+  [dateComponents setSecond:second];
+  
+  return [calendar dateFromComponents:dateComponents];
+}
+
+@end
---
+++ WPSKitTests/WPSKitTests/NSDateTests.m
@@ -0,0 +1,59 @@
+//
+//  NSDateTests.m
+//  WPSKitTests
+//
+//  Created by Kirby Turner on 5/22/15.
+//  Copyright (c) 2015 White Peak Software Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "NSDate+WPSKit.h"
+
+@interface NSDateTests : XCTestCase
+
+@end
+
+@implementation NSDateTests
+
+- (NSDateComponents *)dateComponentsFromDate:(NSDate *)date
+{
+  NSCalendarUnit components = NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay|NSCalendarUnitHour|NSCalendarUnitMinute|NSCalendarUnitSecond;
+  NSCalendar *calendar = [NSCalendar calendarWithIdentifier:NSGregorianCalendar];
+  NSDateComponents *dateComponents = [calendar components:components fromDate:date];
+  return dateComponents;
+}
+
+- (void)testTodayAtMidnight
+{
+  NSDate *date = [NSDate wps_todayAtMidnight];
+  XCTAssertNotNil(date, @"Unassigned NSDate instance.");
+
+  NSDateComponents *dateComponents = [self dateComponentsFromDate:date];
+  XCTAssertEqual([dateComponents hour], 0);
+  XCTAssertEqual([dateComponents minute], 0);
+  XCTAssertEqual([dateComponents second], 0);
+}
+
+- (void)testDateAtMidnight
+{
+  NSDate *date = [[NSDate date] wps_dateAtMidnight];
+  XCTAssertNotNil(date, @"Unassigned NSDate instance.");
+
+  NSDateComponents *dateComponents = [self dateComponentsFromDate:date];
+  XCTAssertEqual([dateComponents hour], 0);
+  XCTAssertEqual([dateComponents minute], 0);
+  XCTAssertEqual([dateComponents second], 0);
+}
+
+- (void)testDateAtHourMinuteDay
+{
+  NSDate *date = [[NSDate date] wps_dateWithHour:13 minute:12 second:11];
+  XCTAssertNotNil(date, @"Unassigned NSDate instance.");
+  
+  NSDateComponents *dateComponents = [self dateComponentsFromDate:date];
+  XCTAssertEqual([dateComponents hour], 13);
+  XCTAssertEqual([dateComponents minute], 12);
+  XCTAssertEqual([dateComponents second], 11);
+}
+
+@end
