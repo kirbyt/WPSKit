234cb26a0d05a45c98f9dc13edfb93964bb70bea
Kirby Turner <kirby@thecave.com>
2016-04-13 16:32:11 -0400

    Add workspace and playground for playing around.

---
+++ WPSKit.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import WPSKit
+
+var str = "Hello, WPSKit playground."
+let color = UIColor.iOSDefaultBlue()
+let anotherColor = UIColor(hex: 0x00ffff, alpha: 0.9)
